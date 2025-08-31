78586ca44434fd90422b3cf1d8ae11efdedb9486
tksn38 <tksn38@pekoro.moe>
2025-08-31 17:02:56 +0900

    modify(install.sh) 202508311702

--- scripts/install_nodejs.sh
+++ scripts/install_nodejs.sh
@@ -2,6 +2,8 @@
 curl -sL https://rpm.nodesource.com/setup_22.x | sudo bash -
 yum install -y gcc-c++ make
 yum install -y nodejs
+npm cache clean --force
+mkdir -p /var/www/html
 npm install -g forever
 cd /var/www/html/
 npm install
